c6174f702e7a7c1daa12068cfacbee9e8e2d4d0d
Elvis Douglas Janegitz <edjelvis@gmail.com>
2019-05-12 22:27:43 -0300

    Adicionando NODE_ENV

--- src/config/database.js
+++ src/config/database.js
@@ -2,9 +2,15 @@ const mongoose = require('mongoose')
 let db
 module.exports = () => {
   if (!db) {
-    db = mongoose.connect('mongodb://3.215.231.192:27017/carteira_virtual', {useNewUrlParser: true})
-      .then(() => console.log('Conectado com o banco de dados!'))
-      .catch(err => console.error(err))
+    if (process.env.NODE_ENV === 'development') {
+      db = mongoose.connect('mongodb://3.215.231.192:27017/carteira_virtual', { useNewUrlParser: true })
+        .then(() => console.log('Conectado com o banco de dados em modo DESENVOLVIMENTO!'))
+        .catch(err => console.error(err))
+    } else {
+      db = mongoose.connect('mongodb://localhost:27017/carteira_virtual', { useNewUrlParser: true })
+        .then(() => console.log('Conectado com o banco de dados em modo PRODUÇÃO!'))
+        .catch(err => console.error(err))
+    }
   }
   return mongoose.connection
 }
